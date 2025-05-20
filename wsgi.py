f43e3e838729788fb9c802c59c58f89b08139aea
kilanko <kpodosky@users.noreply.github.com>
2025-05-20 11:20:46 +0100

    Delete wsgi.py
    Signed-off-by: kilanko <kpodosky@users.noreply.github.com>

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-from Prydict import create_app
-
-# Create the application instance
-app = create_app(os.getenv('FLASK_ENV', 'production'))
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
